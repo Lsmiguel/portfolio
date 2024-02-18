10e245dc6a63cba0a34bc82850d944a5e82997c1
Miguel LS <115990649+Lsmiguel@users.noreply.github.com>
2024-02-18 16:57:19 +0000

    Update homepage layout and add banner image

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import banner from "../../assets/banner/banner.jpg";
 
 export const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <div className="relative">
+      <img src={banner} style={{height: "900px"}} alt="" className="w-full" />
+      <div className="absolute top-0 left-0 z-10 text-center w-full h-screen flex flex-col justify-center items-center">
+        <p>-2024-</p>
+        <div>
+          <h1 className="text-9xl">Olá!</h1>
+          <h1 className="text-7xl">Eu sou o Miguel, bem vindo.</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+{/* <div className="bg-p21 mt-10" style={{height: "200px"}}>
+            <p>tedssdf</p>
+        </div> */}
